2d5344612f892add05cfb90db6aae651b4785565
10soa <soaornella5@gmail.com>
2023-01-17 22:30:02 +0300

    register page

--- src/app/icons/icon-subset.ts
+++ src/app/icons/icon-subset.ts
@@ -88,6 +88,7 @@ import {
   cilUserFemale,
   cilUserFollow,
   cilUserUnfollow,
+  cilPhone,
 } from '@coreui/icons';
 
 export const iconSubset = {
@@ -102,6 +103,7 @@ export const iconSubset = {
   cibLinkedin,
   cibSkype,
   cibTwitter,
+  cilPhone,
   cifBr,
   cifEs,
   cifFr,
@@ -272,4 +274,5 @@ export enum IconSubset {
   cilUserFemale = 'cilUserFemale',
   cilUserFollow = 'cilUserFollow',
   cilUserUnfollow = 'cilUserUnfollow',
+  cilPhone = "cilPhone",
 }
