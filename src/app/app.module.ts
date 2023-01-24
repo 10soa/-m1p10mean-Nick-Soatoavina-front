1a770231eae45de7d256330b0584ef4c7bacf830
10soa <soaornella5@gmail.com>
2023-01-24 23:42:54 +0300

    login integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { HashLocationStrategy, LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import {
   PERFECT_SCROLLBAR_CONFIG,
@@ -86,6 +86,8 @@ const APP_CONTAINERS = [
     ListGroupModule,
     CardModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     {
--- src/app/views/pages/login/login.component.ts
+++ src/app/views/pages/login/login.component.ts
@@ -1,19 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent  implements OnInit {
+  form : FormGroup = this.formBuilder.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required]
+});
+  loading = false;
+  submitted = false;
+  visibleError = false;
+  error = "";
+  dismissible = true;
+  constructor( private http: HttpClient,private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router) { 
+    }
 
-  constructor( private http: HttpClient ) {}
-  login(name: string, mdp:string){
-  this.http.post('http://localhost/Mean_projet/Responsable/login',{ nom: name, mdp:mdp}).subscribe((result: any) => {
-    console.log(result) 
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+        username: ['', Validators.required],
+        password: ['', Validators.required]
+    });
+  }
+
+  get f() { 
+    return this.form.controls; 
+  }
+
+  onAlertVisibleChange(eventValue: any = this.visibleError) {
+    this.visibleError = eventValue;
+  }
+
+  login(){
+    this.submitted = true;
+    this.loading = true;
+  this.http.post('http://localhost/Mean_projet/login',{ nom: this.f['username'].value, mdp:this.f['password'].value}).subscribe((result: any) => {
+  localStorage.setItem('type_user',result.type_user);
+  localStorage.setItem('utilisateur',JSON.stringify(result.utilisateur));
+  this.router.navigateByUrl('/dashboard');
   }, error => {
-    console.log("error")
+    this.visibleError = true;
+    this.error = error.error.message;
+    this.loading = false;
   })
   }
 }
